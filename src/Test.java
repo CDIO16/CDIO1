914e95ffb61e5e10c6b4519b9122674488e20cd4
Hjorthen <Unknown>
2015-09-26 20:21:47 +0200

    Positioned the dices and added code which proves that ColorGrid works

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,27 @@
+import java.awt.Color;
+import desktop_fields.Field;
 import desktop_resources.GUI;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GUI.setDice(3,4);
+		ColorField f = new ColorField.Builder().setBgColor(Color.red).build();
+		Field[] fields = {f};
+		GUI.create(fields);
+		/*
+		
+		
+		FieldFactory.fields.clear();
+		FieldFactory.fields.add(f);
+		//GUI.create(fields);
+		
+		
+		//GUI.setDice(2, 5);
+		 * 
+		 */
+		f.setColor(Color.blue);
+		GUI.setDice(5, 0, 6, 5, 2, 0, 5, 6);
 	}
 
 }
