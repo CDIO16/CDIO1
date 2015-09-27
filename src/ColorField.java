b683c4966ad44720c6ceae30f397a3e9ed095ae5
Hjorthen <Unknown>
2015-09-27 13:55:29 +0200

    Prevented the centerimage from changing when the mouse moved over a field

--- src/ColorField.java
+++ src/ColorField.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.awt.event.MouseListener;
 import desktop_board.Board;
 import desktop_fields.Field;
 import desktop_fields.iBuilder;
@@ -28,12 +28,21 @@ public final class ColorField extends Field {
 		}
 	}
 	
-	  private ColorField(Color bgColor, Color fgColor, String title, String subText,
-		        String description) {
+	  private ColorField(Color bgColor, Color fgColor, String title, String subText, String description) 
+	  {
 		        super(bgColor, fgColor, title, subText, description, null);
+		        
+		        //Removes the mouse listener added by Field.java's constructor. This stops the center tile from changing when you mouse over a tile. 
+		        for(MouseListener ml : layered.getMouseListeners())
+		        {
+		        	layered.removeMouseListener(ml);
+		        }
+		        
+		        
 		    }
 	  public void setColor(Color c)
 	  {
+		 
 		  layered.setForeground(c);
 		  layered.setBackground(c);
 	  }
