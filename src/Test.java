698b55559980213ef8075f45975f47f1fe6a7253
Hjorthen <Unknown>
2015-09-27 13:55:01 +0200

    Removed center image, also found a way to remove car image and potentially replace it!

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,9 @@
 import java.awt.Color;
+import java.awt.Graphics2D;
+
+import desktop_board.Board;
+import desktop_board.Center;
+import desktop_codebehind.Player;
 import desktop_fields.Field;
 import desktop_resources.GUI;
 
@@ -6,9 +11,23 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Hides the questionmark in center. 
+		Center.getInstance().getCenterPanel().getComponent(0).setVisible(false);
+		
+		
 		ColorField f = new ColorField.Builder().setBgColor(Color.red).build();
 		Field[] fields = {f};
 		GUI.create(fields);
+		GUI.addPlayer("Bjarne", 0);
+		GUI.addPlayer("Benny", 0);
+		GUI.setBalance("Bjarne", 5);
+		
+		//Fjerner bilen fra scoreboarded
+		Player plr = Board.getInstance().getPlayer("Bjarne");
+		Graphics2D img = plr.getImage().createGraphics();
+		img.setBackground(new Color(0, 0, 0, 0));
+		img.clearRect(0, 0, plr.getImage().getWidth(),plr.getImage().getHeight());
+		Board.getInstance().updatePlayers();
 		/*
 		
 		
